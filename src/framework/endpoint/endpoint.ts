c746d5b69951a1db8150203c1ad00f9f186643cb
Noah Kim <noahbkim@gmail.com>
2020-12-28 03:08:49 -0500

    Thank god

--- src/framework/endpoint/endpoint.ts
+++ src/framework/endpoint/endpoint.ts
@@ -1,4 +1,27 @@
-export class ReadOnlyResourceEndpoint {
-  public constructor() {
+import {JsonApi} from '../api';
+import {AnyIResource, IType} from '../../schema';
+import {IModel, Model} from '../../model';
+import {PartialResourceQuery} from './query';
+import {ManyDocument} from '../document';
+
+export class ReadOnlyResourceEndpoint<
+  TType extends IType,
+  TIReadResource extends AnyIResource<TType>,
+  TIWriteResource extends AnyIResource<TType> = TIReadResource,
+> {
+  protected api: JsonApi;
+  protected model: IModel<TType, TIReadResource, TIWriteResource>;
+  protected url: string;
+
+  public constructor(api: JsonApi, model: IModel<TType, TIReadResource, TIWriteResource>, url: string) {
+    this.api = api;
+    this.model = model;
+    this.url = url;
+  }
+
+  public getAll(
+    q: PartialResourceQuery<Model<TType, TIReadResource, TIWriteResource>>
+  ): Promise<ManyDocument<Model<TType, TIReadResource, TIWriteResource>>> {
+    return this.api.getAll(this.url, this.model, q);
   }
 }
